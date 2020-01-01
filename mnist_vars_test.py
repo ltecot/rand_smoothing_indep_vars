82b8205b95634aa517244ccc9b6b7ff2b556214e
ltecot <lucastecot@gmail.com>
2019-12-31 18:37:15 -0800

    backup

--- mnist_vars_test.py
+++ mnist_vars_test.py
@@ -2,6 +2,7 @@
 # Similar to mnist_train but modified to optimize sigmas instead.
 
 from mnist_train import Net
+from smoothing import Smooth
 
 from __future__ import print_function
 import argparse
@@ -47,15 +48,6 @@ def train(args, model, device, train_loader, optimizer, epoch, N0, N, alpha, bat
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
 
-def main():
-    
-
-if __name__ == '__main__':
-    main()
-
-# ------------------------------------------------------------------
-
-
 def test(args, model, device, test_loader):
     model.eval()
     test_loss = 0
@@ -63,17 +55,19 @@ def test(args, model, device, test_loader):
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            # output = model(data)
+            prediction, radius = smoothed_classifier.certify(data, N0, N, alpha, batch)
+            # test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            # pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            # correct += pred.eq(target.view_as(pred)).sum().item()
+            test_loss += radius
 
     test_loss /= len(test_loader.dataset)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-
+    # print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    #     test_loss, correct, len(test_loader.dataset),
+    #     100. * correct / len(test_loader.dataset)))
+    print('\nTest set: Average radius: {:.4f}'.format(test_loss)
 
 def main():
     # Training settings
@@ -119,6 +113,8 @@ def main():
                        ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
+    # model = Net().to(device)
+    # optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
     model = Net().to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
@@ -134,3 +130,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+

ee45c616b7b5986198b68928a1fb8ef9dd953b9c
ltecot <lucastecot@gmail.com>
2020-02-06 11:05:15 -0800

    dev

--- mnist_vars_test.py
+++ mnist_vars_test.py
@@ -35,7 +35,7 @@ parser.add_argument('--test-batch-size', type=int, default=1, metavar='N', # 100
                     help='input batch size for testing (default: 1000)')
 parser.add_argument('--epochs', type=int, default=14, metavar='N',
                     help='number of epochs to train (default: 14)')
-parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
+parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
                     help='learning rate (default: 1.0)')
 parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
                     help='Learning rate step gamma (default: 0.7)')

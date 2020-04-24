f3f5702df73f7d23e7aea94078f3d6f835193271
ltecot <lucastecot@gmail.com>
2020-04-23 20:44:58 -0700

    working backprop

--- smoothing.py
+++ smoothing.py
@@ -36,7 +36,7 @@ class Smooth(object):
         else:
             self.sigma = torch.tensor(sigma, requires_grad=True)
             # self.sigma = torch.tensor(sigma, requires_grad=True, device='cuda')
-        self.unit_norm = Normal(torch.tensor([0.0]), torch.tensor([1.0]))
+        self.unit_norm = Normal(torch.tensor([0.0]).cuda(), torch.tensor([1.0]).cuda())
         self.eps = 0.0000001 # To prevent icdf from returning infinity.
         self.indep_vars = indep_vars
 
@@ -105,7 +105,7 @@ class Smooth(object):
         # else:
         # radius = self.sigma * norm.ppf(pABar)
         # print(nA, n)
-        # print(counts_estimation.grad)
+        # print(counts_estimation.type())
         # print(counts_estimation / n)
         if self.indep_vars:
             # print(self.sigma.grad)
@@ -150,39 +150,47 @@ class Smooth(object):
         :param batch_size:
         :return: an ndarray[int] of length num_classes containing the per-class counts
         """
-        with torch.no_grad():
-            if not training:
-                counts = np.zeros(self.num_classes, dtype=int)
-            else:
-                counts = torch.tensor(0.0).cuda()
-            for _ in range(ceil(num / batch_size)):
-                this_batch_size = min(batch_size, num)
-                num -= this_batch_size
+        # with torch.no_grad():
+        if not training:
+            counts = np.zeros(self.num_classes, dtype=int)
+        else:
+            counts = torch.tensor(0.0).cuda()
+        for _ in range(ceil(num / batch_size)):
+            this_batch_size = min(batch_size, num)
+            num -= this_batch_size
 
-                batch = x.repeat((this_batch_size, 1, 1, 1))
-                # print(batch.type())
-                # noise = torch.randn_like(batch, device='cuda') * self.sigma
-                # print(torch.randn_like(batch).get_device())
-                # print(self.sigma.get_device())
-                noise = torch.randn_like(batch) * self.sigma
-                # print(self.sigma.shape)
-                # print(batch.shape)
-                # print(noise.type())
-                # print(torch.exp(self.base_classifier(batch + noise)))
-                # predictions = self.base_classifier(batch + noise).argmax(1)
-                output = self.base_classifier(batch + noise)
-                predictions = output.argmax(1)
-                if not training:
-                    counts += self._count_arr(predictions.cpu().numpy(), self.num_classes)
-                else:
-                    # For now we'll just sum the probs from the truth label class
-                    # counts += torch.sum(predictions == truth_label)
-                    # print(torch.sum(torch.exp(output[:, truth_label])))
-                    # if (output[:, truth_label] != output[:, truth_label]).any():
-                    #     print(output[:, truth_label])
-                    counts += torch.sum(torch.exp(output[:, truth_label]))
-                # print(counts.type())
-            return counts
+            batch = x.repeat((this_batch_size, 1, 1, 1))
+            # print(x.type())
+            # noise = torch.randn_like(batch, device='cuda') * self.sigma
+            # print(torch.randn_like(batch).shape)
+            # print(self.sigma.shape)
+            noise = self.sigma * torch.randn_like(batch)
+            # print(self.sigma.type())
+            # print(batch.shape)
+            # print(noise.shape)
+            # x = torch.randn(5, 5)  # requires_grad=False by default
+            # y = torch.randn(5, 5)  # requires_grad=False by default
+            # z = torch.randn((5, 5), requires_grad=True)
+            # a = x + y
+            # b = a + z
+            # print(b.requires_grad)
+            # print(noise.requires_grad)
+            # print(torch.exp(self.base_classifier(batch + noise)))
+            # predictions = self.base_classifier(batch + noise).argmax(1)
+            output = self.base_classifier(batch + noise)
+            # print(counts.type())
+            predictions = output.argmax(1)
+            if not training:
+                counts += self._count_arr(predictions.cpu().numpy(), self.num_classes)
+            else:
+                # For now we'll just sum the probs from the truth label class
+                # counts += torch.sum(predictions == truth_label)
+                # print(torch.sum(torch.exp(output[:, truth_label])))
+                # if (output[:, truth_label] != output[:, truth_label]).any():
+                #     print(output[:, truth_label])
+                counts += torch.sum(torch.exp(output[:, truth_label]))
+            # print(counts.type())
+        return counts
 
     def _count_arr(self, arr: np.ndarray, length: int) -> np.ndarray:
         counts = np.zeros(length, dtype=int)

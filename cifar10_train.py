4f24057fd52666f8fba8250e5e33cde44c51bbf0
ltecot <lucastecot@gmail.com>
2020-05-06 22:16:45 -0700

    cifar train running, need better model

--- cifar10_train.py
+++ cifar10_train.py
@@ -4,9 +4,10 @@
 # net = Net()
 # net.load_state_dict(torch.load(PATH))
 
+import argparse
 import torch
 import torchvision
-import torchvision.transforms as transforms
+from torchvision import datasets, transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
@@ -96,11 +97,11 @@ def main():
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
                                             shuffle=True, **kwargs)
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                         download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size,
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size,
                                             shuffle=True, **kwargs)
 
     model = Net().to(device)
